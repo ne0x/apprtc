9a07416f543652cd29e55174a26b553ab05b02ed
Alexander Avanesov <chiksot@gmail.com>
2018-09-01 03:32:29 +0300

    pushState change disabled to use nginx proxy > Failed to start signaling: Failed to execute 'pushState' on 'History': A history state object with URL 'http://localhost:8080/r/017587394' cannot be created in a document with origin 'https://...' and URL 'https://.../'.

--- src/web_app/js/appcontroller.js
+++ src/web_app/js/appcontroller.js
@@ -436,10 +436,10 @@ AppController.prototype.onKeyPress_ = function(event) {
 };
 
 AppController.prototype.pushCallNavigation_ = function(roomId, roomLink) {
-  if (!isChromeApp()) {
-    window.history.pushState({'roomId': roomId, 'roomLink': roomLink}, roomId,
-        roomLink);
-  }
+  // if (!isChromeApp()) {
+  //   window.history.pushState({'roomId': roomId, 'roomLink': roomLink}, roomId,
+  //       roomLink);
+  // }
 };
 
 AppController.prototype.displaySharingInfo_ = function(roomId, roomLink) {
